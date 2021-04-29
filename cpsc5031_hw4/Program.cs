2bde709388259aacf1a2cfc3d7aeb3a179966628
dnguyen <david.nguyen@smartsheet.com>
2021-04-29 01:21:54 -0700

    updates

--- cpsc5031_hw4/Program.cs
+++ cpsc5031_hw4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace cpsc5031_hw4
 {
@@ -7,199 +8,118 @@ namespace cpsc5031_hw4
         public static void Main(string[] args)
         {
             Console.WriteLine("Homework 4");
-            int[] unsorted = new int[] { 3, 0, 1, 5 };
-            Console.WriteLine("Unsorted array list");
-            foreach (var item in unsorted)
+            Console.WriteLine("--------------------------------------------------");
+            //Test case 1
+            int[] list1 = new int[1];
+            Console.WriteLine("Test Empty - 1 [] -unsorted");
+            foreach (var item in list1)
             {
                 Console.WriteLine(item);
             }
-            //int[] unsortedA;
-            //int[] unsortedB;
-            var sorted = SplitArray(unsorted);
-            Console.WriteLine("Unsorted array list");
-            foreach (var item in sorted)
+            Console.WriteLine("Splitted Array list and sorted");
+            SplitArray(list1, 0, 0);
+            Console.WriteLine("Test Empty - 1 [] -sorted");
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------------------------------------------------");
+
+            //Test case 2
+            int[] list2 = new int[] { 0, 1, 2, 3 };
+            Console.WriteLine("Test array [0, 1, 2, 3]");
+            foreach (var item in list2)
             {
                 Console.WriteLine(item);
             }
 
-
-
-            //Console.WriteLine("Sorted array list");
-            //var sorted = SortAscending(unsorted);
-            //foreach (var item in sorted)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //Console.WriteLine("Use sort array functions");
-            //var sortedV2 = SortItems(unsorted);
-            //foreach (var item in sortedV2)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //int[] unsortedv2 = new int[] { 3, 0, 1, 5, 8 };
-            //Console.WriteLine("Unsorted array list -List 2");
-            //foreach (var item in unsortedv2)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //var sortedV3 = SortItems(unsortedv2);
-            //Console.WriteLine("Sorted array list - List 2");
-            //foreach (var item in sortedV3)
-            //{
-            //    Console.WriteLine(item);
-            //}
-        }
-
-        public int[] DevideList(int[] list)
-        {
-            int length = list.Length;
-            int[] sorted = new int[length];
-            if (length <= 1)
+            Console.WriteLine("Splitted Array list and sorted");
+            SplitArray(list2, 0, 3);
+            foreach (var item in list2)
             {
-                return list;
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------------------------------------------------");
+
+            //Test case 3
+            int[] list3 = new int[] { 0, 1, 2, 3, 4 };
+            Console.WriteLine("Test array [0, 1, 2, 3, 4]");
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item);
+            }
+            SplitArray(list3, 0, 4);
+            Console.WriteLine("Splitted Array list and sorted");
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------------------------------------------------");
+
+            //Test case 4
+            int[] list4 = new int[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
+            Console.WriteLine("Test array [3, 1, 4, 1, 5, 9, 2, 6, 5]");
+            foreach (var item in list4)
+            {
+                Console.WriteLine(item);
+            }
+            SplitArray(list4, 0, 8);
+            Console.WriteLine("Splitted Array list and sorted");
+            foreach (var item in list4)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+            //Test case 5
+            int[] list5 = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            Console.WriteLine("Test array [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]");
+            foreach (var item in list5)
+            {
+                Console.WriteLine(item);
+            }
+            SplitArray(list5, 0, 9);
+            Console.WriteLine("Splitted Array list and sorted");
+            foreach (var item in list5)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        /// <summary>
+        /// Split the Array into smaller array until the length = 1
+        /// </summary>
+        /// <param name="list">List unsorted items</param>
+        /// <param name="firstIndex">start index of the array</param>
+        /// <param name="lastIndex">end of the index where the array to be splited</param>
+        public static void SplitArray(int[] list, int firstIndex, int lastIndex)
+        {
+            //base case where length of an array is 1
+            if (list.Length <= 1)
+            {
+                return;
             }
             else
             {
-                return DevideList(list);
-            }
-        }
-
-        public static int[] SplitArray(int[] list)
-        {
-            int length = list.Length;
-            int[] sorted = new int[length];
-            if (length <= 1)
-            {
-                return list;
-            }
-            
-            if (CheckEvenNum(length))
-            {
-
-                int tempLenght = list.Length / 2;
-                int[] temp_1 = new int[tempLenght];
-                int[] temp_2 = new int[tempLenght];
-                for (int i = 0; i < tempLenght; i++)
+                if (firstIndex < lastIndex)
                 {
-                    temp_1[i] = list[i];
-                }
-                for (int i = 0; i < tempLenght; i++)
-                {
-                    temp_2[i] = list[tempLenght + i];
-                }
-                var split_1 = SplitArray(temp_1);
-                Sort(sorted, split_1);
-                var split_2 = SplitArray(temp_2);
-                Sort(sorted, split_2);
-                //sort_1.CopyTo(sorted, 0);
-                //sort_2.CopyTo(sorted, tempLenght);
-                //sort_2.CopyTo(sorted, 0);
-                return sorted;
-            }
-            else
-            {
-                double tempLength = list.Length / 2.0;
-                int roundedUpNum = (int)Math.Ceiling(tempLength);
-                int firstLength = roundedUpNum;
-                int secondLength = (int)tempLength;
-                int[] temp_a = new int[firstLength];
-                int[] temp_b = new int[secondLength];
-                for (int i = 0; i < firstLength; i++)
-                {
-                    temp_a[i] = list[i];
-                }
-                for (int i = 0; i < secondLength; i++)
-                {
-                    temp_b[i] = list[roundedUpNum + i];
-                }
-                SplitArray(temp_a);
-                var sort_1 = Sort(sorted, temp_a);
-                SplitArray(temp_b);
-                var sort_2 = Sort(sorted, temp_b);
-                sort_1.CopyTo(sorted, 0);
-                sort_2.CopyTo(sorted, roundedUpNum);
-
-                return sorted;
-            }
-
-        }
-
-        public static int[] Sort(int[] mainList, int[] unsortedList)
-        {
-            for(int i = 0; i < mainList.Length; i++)
-            {
-                for(int j = 0; j < unsortedList.Length; j++)
-                {
-                    if(unsortedList[j] < mainList[i] && i + j < mainList.Length)
+                    //verify if length of the array
+                    if (firstIndex < lastIndex)
                     {
-                        int smaller = unsortedList[j];
-                        int bigger = mainList[i];
-                        mainList[i] = smaller;
-                        mainList[i + j] = bigger;
-                    }
-                    else
-                    {
-                        int smaller = mainList[i];
-                        mainList[i + j] = smaller;
+                        int mid = (firstIndex + lastIndex) / 2;
+                        int secondArrayIndex = mid + 1;
+                        SplitArray(list, firstIndex, mid);
+                        SplitArray(list, secondArrayIndex, lastIndex);
+                        SortAscending(list, secondArrayIndex, lastIndex);
                     }
                 }
             }
-            return mainList;
-        }
-        public static Array SortItems(int[] items)
-        {
-            int length = items.Length;
-            int[] sorted = new int[length];
-            if (CheckEvenNum(items.Length))
-            {
-
-                int tempLenght = items.Length / 2;
-                int[] temp_1 = new int[tempLenght];
-                int[] temp_2 = new int[tempLenght];
-                for (int i = 0; i < tempLenght; i++)
-                {
-                    temp_1[i] = items[i];
-                }
-                for (int i = 0; i < tempLenght; i++)
-                {
-                    temp_2[i] = items[tempLenght + i];
-                }
-
-                var sorted_1 = SortAscending(temp_1);
-                var sorted_2 = SortAscending(temp_2);
-                sorted_1.CopyTo(sorted, 0);
-                sorted_2.CopyTo(sorted, tempLenght);
-            }
-            else
-            {
-                //int[] sortedV2 = new int[length];
-                double tempLength = items.Length / 2.0;
-                int roundedUpNum = (int)Math.Ceiling(tempLength);
-                int firstLength = roundedUpNum;
-                int secondLength = (int)tempLength;
-                int[] temp_a = new int[firstLength];
-                int[] temp_b = new int[secondLength];
-                for (int i = 0; i < firstLength; i++)
-                {
-                    temp_a[i] = items[i];
-                }
-                for (int i = 0; i < secondLength; i++)
-                {
-                    temp_b[i] = items[roundedUpNum + i];
-                }
-                var sorted_a = SortAscending(temp_a);
-                var sorted_b = SortAscending(temp_b);
-                //sorted_a.CopyTo(sortedV2, 0);
-                //sorted_b.CopyTo(sortedV2, roundedUpNum);
-                //var sortedFinal = SortItems(sortedV2);
-                //sortedFinal.CopyTo(sorted, 0);
-                sorted_a.CopyTo(sorted, 0);
-                sorted_b.CopyTo(sorted, roundedUpNum);
-
-            }
-            return sorted;
         }
 
+        /// <summary>
+        /// method to check if the number is an odd number or even number
+        /// </summary>
+        /// <param name="count">an integer number to check</param>
+        /// <returns>true for the number is an even number and false is odd number</returns>
         public static bool CheckEvenNum(int count)
         {
             if (count % 2 == 0)
@@ -213,15 +133,17 @@ namespace cpsc5031_hw4
         }
 
         /// <summary>
-        /// Sort integer items in an array.
+        /// Sort an array based on given indexes. 
         /// </summary>
-        /// <param name="items">list of unsorted integers</param>
-        /// <returns>list of sorted integers</returns>
-        public static Array SortAscending(int[] items)
+        /// <param name="items">list of unsorted items</param>
+        /// <param name="startIndex">start index of the array</param>
+        /// <param name="endIndex">end of the index where the array to be sorted</param>
+        public static void SortAscending(int[] items, int startIndex, int endIndex)
         {
-            for (int i = 0; i < items.Length; i++)
+            for (int i = startIndex; i < endIndex; i++)
             {
-                if (i + 1 < items.Length)
+                //compare value in each index
+                if (i + 1 < endIndex)
                 {
                     if (items[i] > items[i + 1])
                     {
@@ -238,25 +160,9 @@ namespace cpsc5031_hw4
                         items[i] = bigger;
                     }
                 }
-                else
-                {
-                    if (items[i] > items[i - 1])
-                    {
-                        int smaller = items[i - 1];
-                        int bigger = items[i];
-                        items[i - 1] = bigger;
-                        items[i] = smaller;
-                    }
-                    else if (items[i] < items[i - 1])
-                    {
-                        int smaller = items[i];
-                        int bigger = items[i - 1];
-                        items[i - 1] = smaller;
-                        items[i] = bigger;
-                    }
-                }
             }
-            return items;
         }
+
+
     }
 }
